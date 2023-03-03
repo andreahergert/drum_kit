f5c3186ac429e4cc4a1fa185c1e8118537f953cf
Andrea Hergert <ahergert24@hotmail.com>
2023-03-03 11:59:54 -0800

    added touch screen events

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-window.addEventListener('keydown', function (e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if(!audio) return;
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add('playing');
-  });
+function playSound(keyCode) {
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
+}
 
 function removeTransition(e) {
-  if(e.propertyName !== 'transform') return;
+  if (e.propertyName !== 'transform') return;
   this.classList.remove('playing')
 }
 
 const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+keys.forEach(key => {
+  key.addEventListener('transitionend', removeTransition);
+  
+  // Add touchend event listener
+  key.addEventListener('touchend', function (e) {
+    e.preventDefault();
+    const keyCode = this.getAttribute('data-key');
+    playSound(keyCode);
+  });
+});
+
+// Add event listener for both keydown and touchstart events
+window.addEventListener('keydown', function (e) {
+  const keyCode = e.keyCode;
+  playSound(keyCode);
+});
+
+window.addEventListener('touchstart', function (e) {
+  e.preventDefault();
+  const keyCode = e.target.getAttribute('data-key');
+  playSound(keyCode);
+});
